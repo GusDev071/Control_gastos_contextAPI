8c0830de80a699dbf3f428a0d287f06b94633749
GusDev071 <132627325+GusDev071@users.noreply.github.com>
2026-07-27 15:55:09 -0600

    Add category filtering functionality to expense list

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -6,14 +6,16 @@ export default function ExpenseList() {
 
     const {state} = useBudget()
 
-    const isEmpty = useMemo (()=> state.expenses.length === 0, [state.expenses])
+    const filteredExpences = state.currentCategory ? state.expenses.filter(expense => expense.category === state.currentCategory):state.expenses
+
+    const isEmpty = useMemo (()=> filteredExpences.length === 0, [filteredExpences])
 
   return (
-    <div className="mt-10">
+    <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
         {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay gastos</p> : (
             <>
                 <p className="text-gray-600 text-2xl font-bold my-5">Listado de gastos</p>
-                {state.expenses.map(expense =>(
+                {filteredExpences.map(expense =>(
                     <ExpenseDetail
                        key={expense.id}
                        expense={expense}
